df6dfa4ac14ccd2b71557d11c54de3411a3a09c9
Diego Jara <diegomedina99j@gmail.com>
2024-06-14 17:57:08 -0400

    add center and zoom to googlemap

--- src/app/modules/admin/home/entry-components/event-modal/event-modal.component.ts
+++ src/app/modules/admin/home/entry-components/event-modal/event-modal.component.ts
@@ -2,18 +2,54 @@ import { Component, Inject, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { IEvent } from "@modules/admin/home/interface/event.interface";
 import { MatButton } from "@angular/material/button";
-import { GoogleMapsModule } from '@angular/google-maps'
+import { GoogleMapsModule, MapAdvancedMarker } from "@angular/google-maps";
 @Component({
   selector: "app-event-modal",
   standalone: true,
-  imports: [MatButton, GoogleMapsModule],
+  imports: [MatButton, GoogleMapsModule, MapAdvancedMarker],
   templateUrl: "./event-modal.component.html",
 })
 export class EventModalComponent implements OnInit {
+  //TODO: lat-lng change to dinamyc
+  zoom = 15;
+  center: google.maps.LatLngLiteral = { lat: -36.9358114, lng: -73.018882 };
+  options: google.maps.MapOptions = {
+    mapTypeId: "hybrid",
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  };
+  markerPositions: google.maps.LatLngLiteral[] = [{ lat: -36.9358114, lng: -73.018882 }];
+
   constructor(@Inject(MAT_DIALOG_DATA) private _data: { event: IEvent }, private _matDialogRef: MatDialogRef<EventModalComponent>) {}
 
   ngOnInit(): void {
     console.log(this._data);
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
+  }
+
+
+  zoomIn() {
+    if (this.zoom < this.options.maxZoom) this.zoom++;
+  }
+
+  zoomOut() {
+    if (this.zoom > this.options.minZoom) this.zoom--;
+  }
+
+  click(event: google.maps.MapMouseEvent) {
+    console.log(event.latLng.toJSON());
+  }
+
+  addMarker(event: google.maps.MapMouseEvent) {
+    this.markerPositions.push(event.latLng.toJSON());
   }
 
   closeDialog(): void {
