e1919b529bf235af54254aafd2d9bf9a0264abda
Diego Jara <diegomedina99j@gmail.com>
2024-06-14 16:39:07 -0400

    add google maps template

--- src/app/modules/admin/home/entry-components/event-modal/event-modal.component.ts
+++ src/app/modules/admin/home/entry-components/event-modal/event-modal.component.ts
@@ -1,21 +1,16 @@
-import { Component, Inject, OnInit }     from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { IEvent }                        from '@modules/admin/home/interface/event.interface';
-import { MatButton }                     from '@angular/material/button';
-
+import { Component, Inject, OnInit } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { IEvent } from "@modules/admin/home/interface/event.interface";
+import { MatButton } from "@angular/material/button";
+import { GoogleMapsModule } from '@angular/google-maps'
 @Component({
-  selector   : 'app-event-modal',
-  standalone : true,
-  imports    : [
-    MatButton
-  ],
-  templateUrl: './event-modal.component.html',
+  selector: "app-event-modal",
+  standalone: true,
+  imports: [MatButton, GoogleMapsModule],
+  templateUrl: "./event-modal.component.html",
 })
 export class EventModalComponent implements OnInit {
-  constructor(
-    @Inject(MAT_DIALOG_DATA) private _data: { event: IEvent },
-    private _matDialogRef: MatDialogRef<EventModalComponent>
-  ) {}
+  constructor(@Inject(MAT_DIALOG_DATA) private _data: { event: IEvent }, private _matDialogRef: MatDialogRef<EventModalComponent>) {}
 
   ngOnInit(): void {
     console.log(this._data);
