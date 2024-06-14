23cbe7f5dcebfe25eaed908d113ff6645b0c1f97
David Misael Villegas Sandoval <david.misa97@gmail.com>
2024-06-14 14:11:23 -0400

    Add CalendarService with events fetching
    A 'CalendarService' has been implemented in the 'admin/home/components/calendar' module.
    
    src/app/modules/admin/home/components/calendar/calendar.service.ts:
    + Injected HttpClient service to handle the http requests
    + Created getEvents method to fetch the events from the 'api/events' endpoint and map responses to their content.

---
+++ src/app/modules/admin/home/components/calendar/calendar.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Api }        from '@core/interfaces/api';
+import { IEvent }     from '@modules/admin/home/interface/event.interface';
+import { map }        from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalendarService {
+
+  constructor(private _http: HttpClient) { }
+
+  getEvents() {
+    return this._http.get<Api<IEvent[]>>('api/events')
+      .pipe(map(response => response.content));
+  }
+}
