4e476b8701a0837829475be6313f0c748134eda7
David Misael Villegas Sandoval <david.misa97@gmail.com>
2024-06-25 18:13:34 -0400

    Update UI configuration and various components
    In this commit, several changes are made focusing primarily on UI configuration and improvements in various components.
    
    Changes in `app.config.ts`:
    + Changed color scheme from dark to light
    
    Changes in `angular-material.scss`:
    + Added style properties for .mat-mdc-dialog-panel class
    + Set max-height attribute for .mat-mdc-dialog-container class
    
    Changes in `new.component.ts`:
    + Imported MatInputModule and MatDatepickerModule from @angular/material/input and @angular/material/datepicker respectively
    + Replaced MatInput with MatInputModule
    
    Changes in `new.component.html`:
    + Re-arranged and cleaned up the structure of inputs and form fields to increase readability
    + Corrected the order of input and mat-datepicker
    + Adjusted attribute order within the tags for better readability
    
    Changes in `styles.scss`:
    + Removed style properties for .mat-mdc-dialog-container class
    
    Changes in `details.component.html`:
    + Simplified div class attributes for greater readability

--- src/app/modules/admin/admin/events/pages/new/new.component.ts
+++ src/app/modules/admin/admin/events/pages/new/new.component.ts
@@ -7,7 +7,7 @@ import { MatDialogRef }                                                         
 import { takeUntil }                                                             from 'rxjs';
 import { MatButton, MatIconButton }                                              from '@angular/material/button';
 import { MatFormField, MatLabel }                                                from '@angular/material/form-field';
-import { MatInput }                                                              from '@angular/material/input';
+import { MatInputModule }                                                        from '@angular/material/input';
 import { CdkTextareaAutosize }                                                   from '@angular/cdk/text-field';
 import { NgForOf, NgIf }                                                         from '@angular/common';
 import { MatIcon }                                                               from '@angular/material/icon';
@@ -25,7 +25,7 @@ import { MatDatepicker, MatDatepickerInput, MatDatepickerToggle }               
     MatIconButton,
     MatLabel,
     MatFormField,
-    MatInput,
+    MatInputModule,
     CdkTextareaAutosize,
     MatCheckbox,
     MatDatepickerToggle,
