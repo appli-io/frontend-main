ade47f1de579594b78b49cb5492ec7ad85d36644
Diego Jara <diegomedina99j@gmail.com>
2024-06-26 15:10:05 -0400

    refactor: Update new event form design and functionality

--- src/app/modules/admin/admin/events/pages/new/new.component.ts
+++ src/app/modules/admin/admin/events/pages/new/new.component.ts
@@ -17,6 +17,35 @@ import { MatDatepickerModule }                                                  
 import { MatSelectModule }                                                                         from '@angular/material/select';
 import { MatOptionModule }                                                                         from '@angular/material/core';
 import { MatDivider }                                                                              from '@angular/material/divider';
+import {MatStepperModule}                                                                          from '@angular/material/stepper';
+import { trackByFn }                                                                                    from '@libs/ui/utils/utils';
+
+const EventStatusEnum = {
+  DRAFT: 'draft',
+  PUBLISHED: 'published',
+  CANCELLED: 'cancelled',
+  POSTPONED: 'postponed',
+  RESCHEDULED: 'rescheduled',
+  ENDED: 'ended'
+};
+
+const EventTypeEnum = {
+  CONFERENCE: 'conference',
+  MEETUP: 'meetup',
+  WORKSHOP: 'workshop',
+  SEMINAR: 'seminar',
+  WEBINAR: 'webinar',
+  HACKATHON: 'hackathon',
+  CONCERT: 'concert',
+  FESTIVAL: 'festival',
+  EXHIBITION: 'exhibition',
+  FAIR: 'fair',
+  SHOW: 'show',
+  PERFORMANCE: 'performance',
+  COMPETITION: 'competition',
+  GAME: 'game',
+  SPORTS: 'sports',
+};
 
 @Component({
   selector: "app-new",
@@ -39,13 +68,18 @@ import { MatDivider }                                                           
     NgIf,
     NgForOf,
     MatDivider,
+    MatStepperModule,
   ],
   templateUrl: "./new.component.html",
 })
 export class NewComponent implements OnInit {
   eventForm: UntypedFormGroup;
   notyf = new Notyf();
-  
+
+  eventStatusOptions = Object.values(EventStatusEnum);
+  eventTypeOptions = Object.values(EventTypeEnum);
+
+  protected readonly trackByFn = trackByFn;
 
   constructor(
     public readonly _matDialogRef: MatDialogRef<NewComponent>,
@@ -56,30 +90,36 @@ export class NewComponent implements OnInit {
 
   ngOnInit(): void {
     this.eventForm = this._formBuilder.group({
-      title: [undefined, [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
-      description: [undefined, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
-      isAllDay: [ true, [ Validators.required ] ],
-      startDate: [undefined, [Validators.required]],
-      endDate: [undefined],
-      location: [undefined, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
-      url: this._formBuilder.array([]),
-      image: [undefined],
-      capacity: [undefined],
-      organizer: this._formBuilder.group({
-        name: [undefined, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
-        email: [undefined, [Validators.required, Validators.email]],
-        phone: this._formBuilder.group({
-          countryCode: [undefined, [Validators.required]],
-          number: [undefined, [Validators.required, Validators.minLength(7), Validators.maxLength(15)]],
-        }),
+      step1: this._formBuilder.group({
+        title: [undefined, [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+        description: [undefined, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
+        isAllDay: [true, [Validators.required]],
+        startDate: [undefined, [Validators.required]],
+        endDate: [undefined],
       }),
-      type: [undefined, [Validators.required]],
-      status: [undefined, [Validators.required]],
+      step2: this._formBuilder.group({
+        location: [undefined, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
+        url: this._formBuilder.array([]),
+        image: [undefined],
+        capacity: [undefined],
+      }),
+      step3: this._formBuilder.group({
+        organizer: this._formBuilder.group({
+          name: [undefined, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
+          email: [undefined, [Validators.required, Validators.email]],
+          phone: this._formBuilder.group({
+            countryCode: [undefined, [Validators.required]],
+            number: [undefined, [Validators.required, Validators.minLength(7), Validators.maxLength(15)]],
+          }),
+        }),
+        type: [undefined, [Validators.required]],
+        status: [undefined, [Validators.required]],
+      }),
     });
   }
 
   get urlArray(): UntypedFormArray {
-    return this.eventForm.get('url') as UntypedFormArray;
+    return this.eventForm.get('step2.url') as UntypedFormArray;
   }
 
   addUrl(): void {
@@ -103,9 +143,11 @@ export class NewComponent implements OnInit {
     if (this.eventForm.valid) {
       this.eventForm.disable();
 
+      const flattenedData = this.flattenFormData(this.eventForm.value);
+
       try {
         this._eventsService
-          .createEvent(this.eventForm.getRawValue())
+          .createEvent(flattenedData)
           .pipe(takeUntil(this._matDialogRef.afterClosed()))
           .subscribe({
             next: (result) => {
@@ -127,6 +169,14 @@ export class NewComponent implements OnInit {
         this.eventForm.enable();
       }
     }
+  }
+
+  flattenFormData(formData: any): any {
+    return {
+      ...formData.step1,
+      ...formData.step2,
+      ...formData.step3
+    };
   }
 
   remove() {
