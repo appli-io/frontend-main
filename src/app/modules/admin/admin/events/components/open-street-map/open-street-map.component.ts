6d0d8fbff078cf57f4072a9e802b560707b006cc
David Misael Villegas Sandoval <david.misa97@gmail.com>
2024-06-27 18:04:24 -0400

    Update leaflet distribution path and refactor open-street-map component
    In this commit, angular.json and open-street-map.component.ts files have been modified.
    + angular.json: Updated the leaflet distribution path in the input-output configuration to keep leaflet images in correct directory.
    + open-street-map.component.ts: Refactored the code for improved readability. Changed the position of variables declaration to maintain the consistency, removed unnecessary comments and spaces, and also formatted strings and object literals.

--- src/app/modules/admin/admin/events/components/open-street-map/open-street-map.component.ts
+++ src/app/modules/admin/admin/events/components/open-street-map/open-street-map.component.ts
@@ -1,47 +1,40 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from "@angular/core";
-import { LeafletMouseEvent, Map, Marker, map, marker, tileLayer } from "leaflet";
+import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { LeafletMouseEvent, Map, map, Marker, marker, tileLayer }                from 'leaflet';
 
 @Component({
-  selector: "app-open-street-map",
-  standalone: true,
-  imports: [],
-  templateUrl: "./open-street-map.component.html",
-  styleUrls: ["./open-street-map.component.scss"],
+  selector   : 'app-open-street-map',
+  standalone : true,
+  imports    : [],
+  templateUrl: './open-street-map.component.html',
+  styleUrls  : [ './open-street-map.component.scss' ],
 })
 export class OpenStreetMapComponent implements AfterViewInit {
-
-  @ViewChild("map")
+  @Output() locationSelected = new EventEmitter<{ latitude: number, longitude: number }>();
+  neededMaps: boolean = false;
+  leafletMap: Map;
+  currentMarker: Marker;
+  @ViewChild('map')
   private mapContainer: ElementRef<HTMLElement>;
 
-  @Output()
-  locationSelected = new EventEmitter<{ latitude: number, longitude: number }>();
-  
-  neededMaps: boolean = false;
-  leafletMap : Map;
-  currentMarker: Marker;
-
   ngAfterViewInit(): void {
- 
-        const initialState = { lng: -72.938316, lat: -36.9760017, zoom: 17 };
+    const initialState = {lng: -72.938316, lat: -36.9760017, zoom: 17};
 
-        this.leafletMap = map(this.mapContainer.nativeElement).setView(
-          [initialState.lat, initialState.lng],
-          initialState.zoom
-        );
+    this.leafletMap = map(this.mapContainer.nativeElement).setView(
+      [ initialState.lat, initialState.lng ],
+      initialState.zoom
+    );
 
-        const isRetina = window.devicePixelRatio > 1;
-        const baseUrl = "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=3dd80fef8eff420593405a01b0bfa621";
-        const retinaUrl = "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey=3dd80fef8eff420593405a01b0bfa621";
-        const tileLayerUrl = isRetina ? retinaUrl : baseUrl;
+    const isRetina = window.devicePixelRatio > 1;
+    const baseUrl: string = 'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=3dd80fef8eff420593405a01b0bfa621';
+    const retinaUrl: string = 'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey=3dd80fef8eff420593405a01b0bfa621';
+    const tileLayerUrl: string = isRetina ? retinaUrl : baseUrl;
 
-        tileLayer(tileLayerUrl, {
-          attribution:
-            'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a> contributors',
-          maxZoom: 20,
-        }).addTo(this.leafletMap);
+    tileLayer(tileLayerUrl, {
+      maxZoom: 20
+    }).addTo(this.leafletMap);
 
-        // marker([url.latitude, url.longitude]).addTo(this.leafletMap);
-        this.leafletMap.on('click', this.onMapClick.bind(this));
+    // marker([url.latitude, url.longitude]).addTo(this.leafletMap);
+    this.leafletMap.on('click', this.onMapClick.bind(this));
   }
 
   onMapClick(event: LeafletMouseEvent): void {
@@ -51,9 +44,8 @@ export class OpenStreetMapComponent implements AfterViewInit {
     if (this.currentMarker) {
       this.currentMarker.remove();
     }
-    this.currentMarker = marker([latLng.lat, latLng.lng]).addTo(this.leafletMap);
+    this.currentMarker = marker([ latLng.lat, latLng.lng ]).addTo(this.leafletMap);
 
-    this.locationSelected.emit({ latitude: latLng.lat, longitude: latLng.lng });
+    this.locationSelected.emit({latitude: latLng.lat, longitude: latLng.lng});
   }
-
 }
